590c79f68907b9ad735d5f389b907629b3174c2a
lmbernardo7520112 <lmbernardo752011@gmail.com>
2021-05-12 16:10:17 -0300

    Criação do projeto frontend com nextjs.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import Head from 'next/head';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>OneBitFood-LMB</title>
+      </Head>
+    
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
 
 export default MyApp
