d5b3d190a536f51d5b5c0e17840b8c925ca1bfe9
lmbernardo7520112 <lmbernardo752011@gmail.com>
2021-05-15 20:07:46 -0300

    Possível erro em getRestaurants.js

--- components/ListRestaurants/index.js
+++ components/ListRestaurants/index.js
@@ -1,42 +1,13 @@
 import React from 'react';
 import { Container, Row, Col }  from 'react-bootstrap';
 import Restaurant from '../ListRestaurants/Restaurant';
+import getRestaurants from '../../services/getRestaurants'
 
 export default function ListRestaurants() {
- const restaurants = [
-  {
-     'id': 1,
-     'name': 'example 1',
-     'description': 'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
-     'delivery_tax': '5',
-     'image_url': '/restaurant.jpeg',
-     'category_title': 'Cozinha japonesa'
-  },
-  {
-     'id': 2,
-     'name': 'example 2',
-     'delivery_tax': '10',
-     'description': 'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
-     'image_url': '/restaurant.jpeg',
-     'category_title': 'Cozinha mineira'
-  },
-  {
-     'id': 3,
-     'name': 'example 3',
-     'delivery_tax': '15',
-     'description': 'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
-     'image_url': '/restaurant.jpeg',
-     'category_title': 'Cozinha vegana'
-  },
-  {
-     'id': 4,
-     'name': 'example 4',
-     'delivery_tax': '10',
-     'description': 'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
-     'image_url': '/restaurant.jpeg',
-     'category_title': 'Cozinha vegana'
-    }
-]
+   const { restaurants, isLoading, isError } = getRestaurants();
+
+   if(!restaurants)
+    return null;
 
   return (
    <div className='mt-5'>
