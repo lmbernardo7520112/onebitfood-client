bde3446ec221aaefde944a112ca4b26c8d74e40a
lmbernardo7520112 <lmbernardo752011@gmail.com>
2021-05-20 18:31:40 -0300

    Listando categorias fake.

--- components/ListRestaurants/index.js
+++ components/ListRestaurants/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col }  from 'react-bootstrap';
+import { Container, Row, Col, Spinner, Alert }  from 'react-bootstrap';
 import Restaurant from '../ListRestaurants/Restaurant';
 import getRestaurants from '../../services/getRestaurants'
 
@@ -7,15 +7,25 @@ export default function ListRestaurants() {
   
    const { restaurants, isLoading, isError } = getRestaurants();
 
-   if(!restaurants)
-    return null;
+   function renderContent() {
+     if(isError)
+        return <Col><Alert variant='custom-red'>Erro ao carregar</Alert></Col>
+     else if(isLoading)
+      return <Col><Spinner animation='border'/></Col>
+     else if(restaurants.lenght == 0)
+      return <Col>Nenhum restaurante disponível ainda...</Col>
+     else
+      return restaurants.map((restaurant, i) => <Restaurant {...restaurant} key={i}/>)
+   }
+
+   
 
   return (
    <div className='mt-5'>
      <h3 className='fw-bold'>Restaurantes</h3>
      <Row>
-      console.log(restaurants)
-      {restaurants.map((restaurant, i) => <Restaurant {...restaurant} key={i}/>)}
+      {console.log(restaurants)}
+         {renderContent()}
      </Row>
    </div>
 )
